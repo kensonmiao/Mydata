feb0fed46f9c0b9cbaf45789148e124e4b9f687b
Kenson <kenson@miaoo.cat>
2023-03-15 17:35:25 -0400

    change name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-app.get('/proxy', (req, res) => {
+app.get('/getData', (req, res) => {
   const url = Buffer.from(req.query.url, 'base64').toString('utf-8');
   request.get(url, (error, response, body) => {
     if (error) {
@@ -15,5 +15,5 @@ app.get('/proxy', (req, res) => {
 });
 
 app.listen(443, () => {
-  console.log('Proxy API listening on port 443!');
+  console.log('GetData API listening on port 443!');
 });
