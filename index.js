87e521d70fbe1fc845581e89a82ac1cc95dcc156
Kenson <kenson@miaoo.cat>
2023-03-15 18:00:35 -0400

    change port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const request = require('request');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/getData', (req, res) => {
   const url = Buffer.from(req.query.url, 'base64').toString('utf-8');
+  console.log(`Start fetching data from ${url} ....`);
   request.get(url, (error, response, body) => {
     if (error) {
       console.error(error);
@@ -14,6 +16,6 @@ app.get('/getData', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('GetData API listening on port 3000!');
 });
