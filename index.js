c0e535befc5aee699de173ea7c399a1b238079c9
Kenson <kenson@miaoo.cat>
2023-03-18 12:16:09 -0400

    remove useless headers

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const port = process.env.PORT || 3000;
 
 app.get('/getData', (req, res) => {
   const url = Buffer.from(req.query.url, 'base64').toString('utf-8');
-  const headers = req.headers;
+  const headers = { ...req.headers };
+  delete headers.host;
+  if (headers['accept-encoding'] !== undefined && headers['accept-encoding'] !== null) {
+    headers['accept-encoding'] = 'deflate, br';
+  }
   console.log(`Start fetching data from ${url} ....`);
   request.get({url, headers}, (error, response, body) => {
     if (error) {
@@ -19,7 +23,11 @@ app.get('/getData', (req, res) => {
 
 app.post('/getData', (req, res) => {
   const url = Buffer.from(req.query.url, 'base64').toString('utf-8');
-  const headers = req.headers;
+  const headers = { ...req.headers };
+  delete headers.host;
+  if (headers['accept-encoding'] !== undefined && headers['accept-encoding'] !== null) {
+    headers['accept-encoding'] = 'deflate, br';
+  }
   const body = req.body;
 
   request.post({ url, headers, json: body }, (error, response, body) => {
